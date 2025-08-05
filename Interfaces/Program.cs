336cea5d82deedd903a4952195d296acd0db984d
richlaws <1man1job1pay@gmail.com>
2025-08-05 12:19:51 +0100

    Added Interfaces and IMovable implementation

---
+++ Interfaces/Program.cs
@@ -0,0 +1,40 @@
+using System;
+
+namespace Interfaces
+{
+    // Interface declaration
+    public interface IMovable
+    {
+        void Move();
+    }
+
+    // Car class
+    public class Car : IMovable
+    {
+        public void Move()
+        {
+            Console.WriteLine("Car is moving");
+        }
+    }
+
+    // Bicycle class
+    public class Bicycle : IMovable
+    {
+        public void Move()
+        {
+            Console.WriteLine("Bicycle is moving");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IMovable car = new Car();
+            IMovable bicycle = new Bicycle();
+
+            car.Move();       // Outputs: Car is moving
+            bicycle.Move();   // Outputs: Bicycle is moving
+        }
+    }
+}
